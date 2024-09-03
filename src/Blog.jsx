53c2f73590322ad858d2b7512161742b81166cfa
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-09-03 14:56:33 +0545

    commit

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,14 +1,11 @@
 import { Link } from "react-router-dom"
 import Navbar from "./Navbar"
-
 import './Blog.css'
 function Blog(){
     return(
     <>
     <Navbar />
        <section class="blog" id="blog">
-
-<h1 class="heading"> our <span>blog</span> </h1>
 
 <div class="box-container">
 
@@ -18,9 +15,8 @@ function Blog(){
         </div>
         <div class="content">
             <a href="#" class="title">All You Need is Love and More Coffee</a>
-            <span>by admin / 1st august, 2024</span>
             <p>People want to get their daily cup of great-tasting coffee in a relaxing atmosphere. Such customers vary in age, although most of clientele are college students and faculty</p>
-            <a href="#" class="btn">read more</a>
+            <a href="#" class="btn">Read more</a>
         </div>
     </div>
 
@@ -30,9 +26,8 @@ function Blog(){
         </div>
         <div class="content">
             <a href="#" class="title">tasty and refreshing coffee</a>
-            <span>by admin / 21st july, 2024</span>
             <p>My market research shows that these are discerning customers that gravitate towards better tasting coffee.</p>
-            <a href="#" class="btn">read more</a>
+            <a href="#" class="btn">Read more</a>
         </div>
     </div>
 
--- src/Createblog.jsx
+++ src/Createblog.jsx
@@ -5,9 +5,46 @@ import './Createblog.css'
 
 function Createblog() {
     return(
-
         <div>
             <Navbar />
+            <div className="create-blog-container">
+            <h2>Create a New Blog </h2>
+            <form className="blog-form">
+                <div className="form-group">
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        placeholder="Enter title"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="subtitle">Subtitle:</label>
+                    <input
+                        type="text"
+                        id="subtitle"
+                        placeholder="Enter subtitle"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description:</label>
+                    <textarea
+                        id="description"
+                        placeholder="Enter description"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="image">Upload Image:</label>
+                    <input
+                        type="file"
+                        id="image"
+                        accept="image/*"
+                    />
+                </div>
+                <button type="button" className="submit-btn">Create Blog</button>
+            </form>
+
+        </div>
         </div>
 
     )
